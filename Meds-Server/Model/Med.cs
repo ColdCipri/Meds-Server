816d19d95d36ac0d0fc4a5bb6df1bc86f3b48502
ColdCipri <colder.ciprian@gmail.com>
2020-03-13 18:11:46 +0200

    Add project files.

---
+++ Meds-Server/Model/Med.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meds_Server.Model
+{
+    public class Med
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int pieces { get; set; }
+        public string type { get; set; }
+        public string best_before { get; set; }
+        public string base_substance { get; set; }
+        public string base_substance_quantity { get; set; }
+        public string description { get; set; }
+
+    }
+    public class CreateMed : Med
+    {
+
+    }
+
+    public class ReadMed : Med
+    {
+        public ReadMed(SqlDataReader row)
+        {
+            if (row.FieldCount == 2)
+            {
+                id = row["Id"] != null ? Convert.ToInt32(row["Id"]) : 0;
+                name = row["Nume"] != null ? row["Nume"].ToString() : " ";
+            }
+            else
+            {
+                id = row["Id"] != null ? Convert.ToInt32(row["Id"]) : 0;
+                name = row["Nume"] != null ? row["Nume"].ToString() : " ";
+                pieces = row["Buc"] != null ? Convert.ToInt32(row["Buc"]) : 0;
+                type = row["Tip"] != null ? row["Tip"].ToString() : " ";
+                best_before = row["DataExpirarii"] != null ? row["DataExpirarii"].ToString() : " ";
+                base_substance = row["SubstantaBaza"] != null ? row["SubstantaBaza"].ToString() : " ";
+                base_substance_quantity = row["SubstBazaCantitate"] != null ? row["SubstBazaCantitate"].ToString() : " ";
+                description = row["Descriere"] != null ? row["Descriere"].ToString() : " ";
+            }
+        }
+
+    }
+}
---
+++ Meds-Server/Utils/QueryMeds.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meds_Server.Utils
+{
+    public static class QueryMed
+    {
+        private static string query_select_Id_Name = "SELECT ID, Nume FROM Medicamente";
+
+        private static string query_select_Id = "SELECT * FROM Medicamente WHERE id = ";
+
+        private static string query_insert = "INSERT INTO Medicamente(Nume, Buc, Tip, DataExpirarii, SubstantaBaza, SubstBazaCantitate, Descriere) " +
+                                    "VALUES (@name, @pieces, @type, @best_before, @base_substance, @base_substance_quantity, @description)";
+
+        private static string query_minimum_insert = "INSERT INTO Medicamente(Nume, Buc, Tip, DataExpirarii) VALUES (@name, @pieces, @type, @best_before)";
+
+        private static string query_update = "UPDATE Medicamente SET name=@name, pieces=@pieces, type=@type, best_before=@best_before, base_substance=@base_substance, " +
+                                    "base_substance_quantity=@base_substance_quantity, description=@description WHERE id=";
+
+        private static string query_delete = "DELETE FROM Medicamente WHERE id=";
+
+        private static string order_by_name = " ORDER BY Nume";
+
+        private static string filter_by_best_before = "DataExpirarii < (select GETDATE())";
+
+
+        public static string GetConnectionString()
+        {
+            return @"Data Source = CIPRI-ROG\SQLEXPRESS; " +
+                    "Initial Catalog = Medicamente_DB; " +
+                    "Integrated Security = True; " +
+                    "MultipleActiveResultSets = True;";
+        }
+
+        public static string getIdAndName() { return query_select_Id_Name; }
+
+        public static string getId() { return query_select_Id; }
+
+        public static string getInsert() { return query_insert; }
+
+        public static string getMinimumInsert() { return query_minimum_insert; }
+
+        public static string getUpdate() { return query_update; }
+
+        public static string getDelete() { return query_delete; }
+
+        public static string getFilterBestBefore() { return filter_by_best_before; }
+
+        public static string getOrderName() { return order_by_name; }
+    }
+}
